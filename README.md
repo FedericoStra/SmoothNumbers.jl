6fa12aa6598c3d7e9c290890a204612c9d5b4e07
Federico Stra <stra.federico@gmail.com>
2023-05-05 17:19:38 +0200

    Initial implementation

--- README.md
+++ README.md
@@ -1,7 +1,53 @@
-# SmoothNumbers
+# SmoothNumbers.jl
+
+> Algorithms to generate smooth numbers
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://FedericoStra.github.io/SmoothNumbers.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://FedericoStra.github.io/SmoothNumbers.jl/dev/)
 [![Build Status](https://github.com/FedericoStra/SmoothNumbers.jl/actions/workflows/CI.yml/badge.svg?branch=master)](https://github.com/FedericoStra/SmoothNumbers.jl/actions/workflows/CI.yml?query=branch%3Amaster)
 [![Code Style: Blue](https://img.shields.io/badge/code%20style-blue-4495d1.svg)](https://github.com/invenia/BlueStyle)
 [![ColPrac: Contributor's Guide on Collaborative Practices for Community Packages](https://img.shields.io/badge/ColPrac-Contributor's%20Guide-blueviolet)](https://github.com/SciML/ColPrac)
+
+See the definition of *smooth number* on
+[Wikipedia](https://en.wikipedia.org/wiki/Smooth_number) and
+[MathWorld](https://mathworld.wolfram.com/SmoothNumber.html).
+
+## Examples
+
+Compute the first 10 3-smooth numbers, i.e. numbers of the form `2^i * 3^j`:
+
+```julia
+julia> using SmoothNumbers
+
+julia> smooth(3, 10)
+10-element Vector{Int64}:
+  1
+  2
+  3
+  4
+  6
+  8
+  9
+ 12
+ 16
+ 18
+```
+
+Compute the first 10 numbers of the form `2^3 * 3^i * 7^i`:
+
+```julia
+julia> using SmoothNumbers
+
+julia> with_bases([2, 3, 7], 10)
+10-element Vector{Int64}:
+  1
+  2
+  3
+  4
+  6
+  7
+  8
+  9
+ 12
+ 14
+ ```
--- src/SmoothNumbers.jl
+++ src/SmoothNumbers.jl
@@ -1,5 +1,123 @@
 module SmoothNumbers
 
-# Write your package code here.
+import Primes
+
+export pratt, smooth, with_bases
+
+"""
+Computes smooth numbers.
+"""
+function smooth end
+
+"""
+    smooth(k::Integer, n::Integer)
+
+Computes the first `n` `k`-smooth numbers and returns them as a `Vector{typeof(k)}`.
+"""
+smooth(k::Integer, n::Integer) = smooth(typeof(k), k, n)
+
+"""
+    smooth(::Type{T}, k::Integer, n::Integer) where T<:Integer
+
+Computes the first `n` `k`-smooth numbers and returns them as a `Vector{T}`.
+"""
+function smooth(::Type{T}, k::Integer, n::Integer) where T<:Integer
+    primes = convert(Vector{T}, Primes.primes(Int(k)))
+    with_bases(primes, n)
+end
+
+"""
+Computes numbers that can be written as a product of powers of a given set of bases.
+"""
+function with_bases end
+
+"""
+    with_bases(bases::AbstractVector{T}, n::Integer) where T<:Integer
+
+Computes the first `n` numbers that can be written as a product of powers of the `bases`.
+
+The result is a `Vector` with the same element type as `bases`.
+"""
+function with_bases(bases::AbstractVector{T}, n::Integer) where T<:Integer
+    if n <= 0
+        return T[]
+    end
+
+    num_bases = length(bases)
+
+    if isempty(bases)
+        return T[1]
+    end
+
+    if num_bases == 1
+        v = Vector{T}(undef, n)
+        x = T(1)
+        v[1] = x
+        for write_index in 2:n
+            x *= bases[1]
+            v[write_index] = x
+        end
+        return v
+    end
+
+    v = Vector{T}(undef, n)
+    indices = ones(Int, num_bases)
+    v[1] = T(1)
+
+    for write_index in 2:n
+        new = minimum(p * v[j] for (p, j) in zip(bases, indices))
+        v[write_index] = new
+        for j in eachindex(bases)
+            if bases[j] * v[indices[j]] == new
+                indices[j] += 1
+            end
+        end
+    end
+
+    v
+end
+
+"""
+Computes 3-smooth numbers.
+"""
+function pratt end
+
+"""
+    pratt(n::Integer)
+
+Computes the first `n` 3-smooth numbers and returns them as a `Vector{Int}`.
+"""
+pratt(n::Integer) = pratt(Int, n)
+
+"""
+    pratt(::Type{T}, n::Integer) where T<:Integer
+
+Computes the first `n` 3-smooth numbers and returns them as a `Vector{T}`.
+"""
+function pratt(::Type{T}, n::Integer) where T<:Integer
+    if n <= 0
+        return T[]
+    end
+
+    v = T[1]
+    two = 1
+    three = 1
+    for _ in 1:n-1
+        times_two = T(2) * v[two]
+        times_three = T(3) * v[three]
+        if times_two < times_three
+            push!(v, times_two)
+            two += 1
+        elseif times_two == times_three
+            push!(v, times_two)
+            two += 1
+            three += 1
+        else
+            push!(v, times_three)
+            three += 1
+        end
+    end
+    v
+end
 
 end
